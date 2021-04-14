a6a9bfaa48ae4a806694b42b8011e538642f683b
agatalb <agatalb@users.noreply.github.com>
2021-04-14 23:15:23 +0200

    managing data update

--- manage_data.R
+++ manage_data.R
@@ -1,6 +1,24 @@
 file = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
-salary_data = read.csv(file)
+#data
+dataset_salary = read.csv(file, header=FALSE)
+
+names(dataset_salary) = c("age","workclass","fnlwgt", "education","educational-num","marital-status",
+                          "occupation","relationship","race","gender","capital-gain","capital-loss",
+                          "hours-per-week","native-country","income")
+
+head(dataset_salary,10)
+
+mean(dataset_salary$age)
+
+#description of sample
+mean(dataset_salary$age)
+
+table(dataset_salary$gender)
+table(dataset_salary$income)
+table(dataset_salary$relationship)
 
 
 
 
+
+#
