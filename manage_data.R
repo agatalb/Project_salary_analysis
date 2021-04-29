8504e97af1a9cce84747003685eb742656087ddd
nataliagraczyk <natalia.graczyk.97@gmail.com>
2021-04-29 22:44:05 +0200

    clean the data

--- manage_data.R
+++ manage_data.R
@@ -9,7 +9,6 @@ names(Data)
 NewData = subset(Data, select = c("WOJ","B2","B4","B5","B6","B7","WIEK","STAZ_OGOL","TD5","TWO_ROK") )
 View(NewData)
 
-
 #descriptive statistics of the data
 summary(NewData)
 
@@ -18,27 +17,28 @@ str(NewData)
 
 ###### THE DATA CLEANING PROCESS #####
 
-# chr -> factor
+# chr -> factor/numeric variable
 
-dataset_salary$workclass <- as.factor(dataset_salary$workclass)
-dataset_salary$education <- as.factor(dataset_salary$education)
-dataset_salary$`marital-status` <- as.factor(dataset_salary$`marital-status`)
-dataset_salary$occupation <- as.factor(dataset_salary$occupation)
-dataset_salary$relationship <- as.factor(dataset_salary$relationship)
-dataset_salary$race <- as.factor(dataset_salary$race)
-dataset_salary$gender<- as.factor(dataset_salary$gender)
-dataset_salary$`native-country` <- as.factor(dataset_salary$`native-country`)
-dataset_salary$income <- as.factor(dataset_salary$income)
+NewData$WOJ <- as.factor(NewData$WOJ)
+NewData$B2 <- as.factor(NewData$B2)
+NewData$B4 <- as.factor(NewData$B4)
+NewData$B5 <- as.factor(NewData$B5)
+NewData$B6 <- as.factor(NewData$B6)
+NewData$B7 <- as.factor(NewData$B7)
+NewData$WIEK <- as.factor(NewData$WIEK)
+NewData$STAZ_OGOL <- as.numeric(NewData$STAZ_OGOL)
+NewData$TD5 <- as.numeric(NewData$TD5)
+NewData$TWO_ROK<- as.numeric(NewData$TWO_ROK)
 
 # how many NAs do we have?
 
-sapply(dataset_salary,function(x) sum(is.na(x)))
+sapply(NewData,function(x) sum(is.na(x)))
 # 0 NA's
 
 # let us see what is the distribution of the income indicator in the dataset.
 
 par(mfrow = c(1,1))
-barplot(table(dataset_salary$income), main = "Frequency of Having Income over 50k")
+plot(table(NewData$TWO_ROK), main = "SALARY")
 
 #decomposition of variables 
 hist(dataset_salary$age)
@@ -47,4 +47,3 @@ hist(dataset_salary$`hours-per-week`)
 hist(dataset_salary$`capital-gain`)
 hist(dataset_salary$`capital-loss`)
 
-usethis::use_git()
